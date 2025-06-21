cf122f0136bb60c2bd2633295e76d30268a0bb01
Dokioli <harshbhise2502@gmail.com>
2025-06-21 14:26:51 +0530

    Created Blend Tree For Charater Movement

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -7,4 +7,34 @@ public class PlayerMovement : MonoBehaviour
 
     [SerializeField] Joystick playerJoystick;
     [SerializeField] float speed;
+
+    Vector2 movement;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        movement.x = playerJoystick.Horizontal;
+        movement.y = playerJoystick.Vertical;
+    }
+    private void FixedUpdate()
+    {
+        if (movement == Vector2.zero)
+        {
+            animator.SetFloat("Move X", 0);
+            animator.SetFloat("Move Y", 0);
+            animator.SetBool("IsMoving", false);
+        }
+        else
+        {
+            rb.linearVelocity = movement * speed;
+            animator.SetFloat("Move X", movement.x);
+            animator.SetFloat("Move Y", movement.y);
+            animator.SetBool("IsMoving", true);
+        }
+    }
 }
